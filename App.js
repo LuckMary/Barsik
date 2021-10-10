115051ed41e7d7f7030e129ccc137de5b1ab2604
Mary Lukicheva <luckmary8@gmail.com>
2021-10-10 20:01:33 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+      }}
+    >
+      <Text>jdhjsgjsjbdj</Text>
+    </View>
+  )
+}
+export default App
+
+console.log('jbhhbukb')
